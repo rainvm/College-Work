3fec273c45d0ece4841a1ec0996f6be2280361f8
rainvm <coyne.ryan3@gmail.com>
2024-01-29 14:40:08 -0500

    Update

---
+++ Physics/Experimental 1/Homework 1/main.py
@@ -0,0 +1,13 @@
+def main():
+    p_target = 1/2
+    r_target = 5.4e3
+    p = 1/8
+    resistors = [10, 15, 22, 33, 47, 68, 100, 150, 220, 330, 470, 680, 1000, 1500, 2200, 3300, 4700, 6800, 10000, 15000, 22000,
+         33000, 47000, 68000]
+    resistance = 0
+    while (resistance - r_target) > 
+
+
+
+if __name__ == '__main__':
+    main()
